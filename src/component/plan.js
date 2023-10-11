e740d5979d6db8742a32eef9ba3bd25efab5689d
Hramn1 <stmihailov@yandex.ru>
2023-10-12 01:26:32 +0300

    add

--- src/component/plan.js
+++ src/component/plan.js
@@ -8,12 +8,20 @@ export class Plan {
         this.btnDeleteElement = document.querySelectorAll('.figure__button-delete');
         this.objElements = this.planGrid.querySelectorAll('.objects__item');
         this.size = 66;
+        this.button = '    <svg class="figure__button figure__button-rotate" fill="currentColor" height="1em" width="1em">\n' +
+            '      <use xlink:href="#rotate-icon"></use>\n' +
+            '    </svg>\n' +
+            '\n' +
+            '    <svg class="figure__button figure__button-delete" fill="currentColor" height="1em" width="1em">\n' +
+            '      <use xlink:href="#delete-icon"></use>\n' +
+            '    </svg>'
     }
     createElements(element){
         console.log(element)
         let p_prime = element.cloneNode(true);
-        this.planGrid.append(p_prime)
-
+        p_prime.removeChild(p_prime.querySelector('.object__name'));
+        p_prime.children[0].children[0].innerHTML = this.button
+        this.planGrid.append(p_prime);
     }
     deleteElements(element){
         this.objElements.forEach((item)=>{
