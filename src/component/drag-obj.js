c2a2ea5cf626dc63cf918d9fe7b2e6c06b1bf269
Hramn1 <stmihailov@yandex.ru>
2023-11-28 01:32:04 +0300

    add three container

--- src/component/drag-obj.js
+++ src/component/drag-obj.js
@@ -17,14 +17,12 @@ export class DragObj {
                 this.yCord = evt.clientY - rect.top;  //y position within the element.
                 if(elementsInDrag.sizeOne.includes(evt.target.dataset.id)){
                     this.size = 1;
-                } else if(elementsInDrag.sizeOne.includes(evt.target.dataset.id)){
+                } else if(elementsInDrag.sizeThree.includes(evt.target.dataset.id)){
                     this.size = 3;
                 } else {
                     this.size = 2;
                 }
                 evt.dataTransfer.setData('id', evt.target.dataset.id);
-                // evt.dataTransfer.setData('x', x.toString());
-                // evt.dataTransfer.setData('y', y.toString());
             })
         })
     }
@@ -49,9 +47,39 @@ export class DragObj {
                                 it.classList.add('plan__cell_success');
                             }
                         })
-                    }
+                    } else if(this.size === 3 && this.xCord <= 66){
+                        this.planCell.forEach((item)=> {
+                            if(Number(item.dataset.x) === Number(it.dataset.x) + 1 && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                                it.classList.add('plan__cell_success');
+                            }
+                            if(Number(item.dataset.x) === Number(it.dataset.x) + 2 && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                            }
+                        })
+                    } else if(this.size === 3 && 66 < this.xCord && this.xCord <= 132){
+                        this.planCell.forEach((item)=> {
+                            if(Number(item.dataset.x) === Number(it.dataset.x) - 1  && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                                it.classList.add('plan__cell_success');
+                            }
+                            if(Number(item.dataset.x) === Number(it.dataset.x) + 1 && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                            }
+                        })
+                    }  else if(this.size === 3 && this.xCord > 132){
+                        this.planCell.forEach((item)=> {
+                            if(Number(item.dataset.x) === Number(it.dataset.x) - 2  && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                                it.classList.add('plan__cell_success');
+                            }
+                            if(Number(item.dataset.x) === Number(it.dataset.x) -1  && Number(item.dataset.y === it.dataset.y)){
+                                item.classList.add('plan__cell_success');
+                            }
+                        })
                 }
-            });
+    }
+    })
         })
     }
     dragleave(){
@@ -74,6 +102,26 @@ export class DragObj {
                             item.classList.remove('plan__cell_success');
                         }
                     })
+                } else if(this.size === 3 && this.xCord <= 66){
+                    it.classList.remove('plan__cell_success');
+                    this.planCell.forEach((item)=> {
+                        if(Number(item.dataset.y === it.dataset.y)){
+                            item.classList.remove('plan__cell_success');
+                        }
+                    })
+                }  else if(this.size === 3 && 66 < this.xCord && this.xCord <= 132){
+                    it.classList.remove('plan__cell_success');
+                    this.planCell.forEach((item)=> {
+                        if(Number(item.dataset.y === it.dataset.y)){
+                            item.classList.remove('plan__cell_success');
+                        }
+                    })
+                }  else if(this.size === 3 && this.xCord > 132){
+                    this.planCell.forEach((item)=> {
+                        if(Number(item.dataset.y === it.dataset.y)){
+                            item.classList.remove('plan__cell_success');
+                        }
+                    })
                 }
             });
         })
@@ -86,11 +134,21 @@ export class DragObj {
             const elementDrop = Array.from(this.elementsDrag).filter((item)=> item.children[0].dataset.id === evt.dataTransfer.getData('id'));
             evt.stopPropagation();
             if(evt.target.classList.contains('plan__cell_success')) {
-                if(this.size === 2 && this.xCord > 66){
+                if(this.size === 2 && this.xCord > 66) {
                    evt.target.previousElementSibling.classList.remove('plan__cell_success')
-                } else if(this.size === 2 && this.xCord <= 66){
+                } else if(this.size === 2 && this.xCord <= 66) {
                     evt.target.nextElementSibling.classList.remove('plan__cell_success')
+                } else if(this.size === 3 && this.xCord <= 66){
+                    evt.target.nextElementSibling.classList.remove('plan__cell_success')
+                    evt.target.nextElementSibling.nextElementSibling.classList.remove('plan__cell_success')
+                } else if(this.size === 3 && this.xCord > 66 && this.xCord <= 132){
+                    evt.target.nextElementSibling.classList.remove('plan__cell_success')
+                    evt.target.previousElementSibling.classList.remove('plan__cell_success')
+                } else if(this.size === 3 && this.xCord > 132){
+                    evt.target.previousElementSibling.classList.remove('plan__cell_success')
+                    evt.target.previousElementSibling.previousElementSibling.classList.remove('plan__cell_success')
                 }
+
                 planContainer.createElements(elementDrop[0], evt.target, this.xCord, this.size);
                 evt.target.classList.remove('plan__cell_success');
             }
--- src/component/plan.js
+++ src/component/plan.js
@@ -1,37 +1,46 @@
-export class Plan {
+import {buttonOneSize, buttonSize} from "../data";
 
+export class Plan {
     constructor(planGrid, planCell) {
         this.planGrid = planGrid;
         this.planCell = planCell;
         this.planCellBusy = [];
         this.btnReset = document.querySelector('.scheduler__action-reset');
         this.size = 66;
-        this.button = `<svg class="figure__button figure__button-rotate" fill="currentColor" height="1em" width="1em"> 
-                        <use xlink:href="#rotate-icon"></use> 
-                       </svg>
-                       <svg class="figure__button figure__button-delete" fill="currentColor" height="1em" width="1em">
-                        <use xlink:href="#delete-icon"></use>
-                       </svg>`;
-        this.buttonOneSize = `<svg class="figure__button figure__button-delete" fill="currentColor" height="1em" width="1em">
-                        <use xlink:href="#delete-icon"></use>
-                       </svg>`
     }
     createElements(element, dropElement, xCord, size){
         let elementClone = element.cloneNode(true);
         elementClone.removeChild(elementClone.querySelector('.object__name'));
         elementClone.children[0].removeAttribute('draggable');
         if(size === 1){
-            elementClone.children[0].children[0].innerHTML = this.buttonOneSize;
+            elementClone.children[0].children[0].innerHTML = buttonOneSize;
             elementClone.style.left = (dropElement.dataset.x - 1) * this.size + 'px';
             elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
         } else if(size === 2 && xCord > 66){
-            elementClone.children[0].children[0].innerHTML = this.button;
+            elementClone.children[0].children[0].innerHTML = buttonSize;
             elementClone.style.left = (dropElement.dataset.x - 2) * this.size + 'px';
             elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
+            elementClone.querySelector('.figure__button-rotate').addEventListener('click', this.checkPossibilityRotate)
         } else if(size === 2 && xCord <= 66) {
-            elementClone.children[0].children[0].innerHTML = this.button;
+            elementClone.children[0].children[0].innerHTML = buttonSize;
             elementClone.style.left = (dropElement.dataset.x - 1) * this.size + 'px';
             elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
+            elementClone.querySelector('.figure__button-rotate').addEventListener('click', this.checkPossibilityRotate)
+        } else if(size === 3 && xCord <= 66) {
+            elementClone.children[0].children[0].innerHTML = buttonSize;
+            elementClone.style.left = (dropElement.dataset.x - 1) * this.size + 'px';
+            elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
+            elementClone.querySelector('.figure__button-rotate').addEventListener('click', this.checkPossibilityRotate)
+        } else if(size === 3 && xCord > 66 && xCord <= 132) {
+            elementClone.children[0].children[0].innerHTML = buttonSize;
+            elementClone.style.left = (dropElement.dataset.x - 2) * this.size + 'px';
+            elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
+            elementClone.querySelector('.figure__button-rotate').addEventListener('click', this.checkPossibilityRotate)
+        } else if(size === 3 && xCord > 132) {
+            elementClone.children[0].children[0].innerHTML = buttonSize;
+            elementClone.style.left = (dropElement.dataset.x - 3) * this.size + 'px';
+            elementClone.style.top = (dropElement.dataset.y - 1) * this.size + 'px';
+            elementClone.querySelector('.figure__button-rotate').addEventListener('click', this.checkPossibilityRotate)
         }
 
         this.planGrid.append(elementClone);
@@ -51,30 +60,37 @@ export class Plan {
             item.remove();
         })
     }
-    checkPossibilityRotate(){
-        console.log(this.planCellBusy)
+    checkPossibilityRotate(evt){
+        let elementTarget =  evt.currentTarget.parentNode.parentNode.parentNode;
+                if(!elementTarget.classList.contains('objects__item-rotated')){
+                    elementTarget.classList.add('objects__item-rotated');
+                } else {
+                    elementTarget.classList.remove('objects__item-rotated');
+                }
     }
+
     updatePlan() {
         const btnDeleteElement = document.querySelectorAll('.figure__button-delete');
-        const btnRotateElement = document.querySelectorAll('.figure__button-rotate');
+
         btnDeleteElement.forEach((item)=>{
             item.addEventListener('click',(evt)=> {
                 evt.currentTarget.parentNode.parentNode.parentNode.remove();
             })
         })
+
+    }
+    init(){
+        const btnRotateElement = document.querySelectorAll('.figure__button-rotate');
         btnRotateElement.forEach((item)=>{
             item.addEventListener('click',(evt)=> {
                 let elementTarget =  evt.currentTarget.parentNode.parentNode.parentNode;
                 if(!elementTarget.classList.contains('objects__item-rotated')){
                     elementTarget.classList.add('objects__item-rotated');
-                    this.checkPossibilityRotate()
                 } else {
                     elementTarget.classList.remove('objects__item-rotated');
                 }
             })
         })
-    }
-    init(){
         this.updatePlan();
         this.planCell.forEach((cell)=>{
             if(cell.classList.contains('plan__cell_error')){
