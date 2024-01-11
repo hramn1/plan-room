db1e9c5fd34584efb5372f6e5d1d3e6402fa0eb3
Stas <stanislav.m@itechpsp.com>
2024-01-11 04:26:06 +0300

    edit

--- src/component/plan.js
+++ src/component/plan.js
@@ -13,19 +13,19 @@ export class Plan {
     const elementClone = element.cloneNode(true);
     elementClone.removeChild(elementClone.querySelector('.object__name'));
     elementClone.children[0].removeAttribute('draggable');
+
     if (size === 1) {
       createElementOnPlan(elementClone, dropElement, xCord, this.size, size);
-      this.planCellBusy.push([Number(dropElement.dataset.x), Number(dropElement.dataset.y)]);
+      this.planCellBusy.push({'id':Number(elementClone.dataset.id), 'cell': [Number(dropElement.dataset.x), Number(dropElement.dataset.y)]});
     } else {
       createElementOnPlan(elementClone, dropElement, xCord, this.size, size);
       elementClone.querySelector('.figure__button-rotate').addEventListener('click',(evt) => this.checkPossibilityRotate(evt, dropElement, size, elementClone));
       if(size === 2 && xCord <= 66){
-        this.planCellBusy.push([Number(dropElement.dataset.x), Number(dropElement.dataset.y)]);
-        this.planCellBusy.push([Number(dropElement.dataset.x + 1) , Number(dropElement.dataset.y)]);
+        this.planCellBusy.push({'id':Number(elementClone.dataset.id), 'cell': [[Number(dropElement.dataset.x), Number(dropElement.dataset.y)], [Number(dropElement.dataset.x + 1), Number(dropElement.dataset.y)]]});
       }
       if(size === 2 && xCord > 66){
-        this.planCellBusy.push([Number(dropElement.dataset.x), Number(dropElement.dataset.y)]);
-        this.planCellBusy.push([Number(dropElement.dataset.x - 1) , Number(dropElement.dataset.y)]);
+        this.planCellBusy.push({'id':Number(elementClone.dataset.id), 'x': Number(dropElement.dataset.x), 'y': Number(dropElement.dataset.y)});
+        this.planCellBusy.push({'id':Number(elementClone.dataset.id), 'x': Number(dropElement.dataset.x -1), 'y': Number(dropElement.dataset.y)});
       }
       if(size === 3 && xCord <= 66){
         this.planCellBusy.push([Number(dropElement.dataset.x), Number(dropElement.dataset.y)]);
@@ -59,25 +59,32 @@ export class Plan {
     });
     objElementsPlan.forEach((item) => {
       item.remove();
+      this.planCellBusy = [];
     });
   }
 
   checkPossibilityRotate(evt, cell, size, element) {
+    let arrP = [(parseInt(element.style.left) / 66) + 1, (parseInt(element.style.top) / 66) + size];
     if(size === 2){
-      let arrP = [(parseInt(element.style.left) / 66) + 1, (parseInt(element.style.top) / 66) + size];
-      if(this.planCellBusy.some((item)=> isEqual(item, arrP))){
+
+      if(this.planCellBusy.some((item)=> isEqual(item.cell, arrP))) {
         element.classList.add('shake-on-hover');
         setTimeout(() => element.classList.remove('shake-on-hover'), 400)
         return;
       }
-
     }
     if (Number(cell.dataset.y) + Number(size) > 7){
       element.classList.add('shake-on-hover');
-      setTimeout(()=>      element.classList.remove('shake-on-hover'), 400)
+      setTimeout(() => element.classList.remove('shake-on-hover'), 400)
       return;
     }
     const elementTarget = evt.currentTarget.parentNode.parentNode.parentNode;
+    if(size === 2) {
+
+    }
+    console.log(element)
+    console.log(this.planCellBusy)
+    console.log(arrP)
     if (!elementTarget.classList.contains('objects__item-rotated')) {
       elementTarget.classList.add('objects__item-rotated');
     } else {
